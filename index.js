67950cc68a765b06767da38624ef4a5e8e6793c5
kaankmk <kaan@kmk.net.tr>
2024-07-29 16:20:56 +0300

    finish

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ document.addEventListener("DOMContentLoaded", function () {
   const successText = statusContainer.querySelector(".success-text");
   const errorText = statusContainer.querySelector(".error-text");
   const progressBar = document.getElementById("progress-bar");
-  const progressTexts = document.querySelector(".progress-texts");
   const progressContainer = progressBar.parentNode;
   const mainContent = document.querySelectorAll(
     "#main-content-1, #main-content-2, #main-content-3"
-  ); // Tüm içerik alanları
+  );
+  const progressMessage = document.getElementById("progress-message");
 
-  let dotsInterval;
-
-  // Başlangıçta tüm içerikleri gizle
-  mainContent.forEach((content) => content.classList.add("d-none"));
+  const messages = [
+    "Bağlantı Kontrolü",
+    "Veritabanı Kontrolü",
+    "Servis Durumları",
+    "Performans Analizi",
+    "Güvenlik Taraması",
+  ];
 
   function updateStatus(isSuccess) {
     if (isSuccess) {
@@ -29,25 +32,45 @@ document.addEventListener("DOMContentLoaded", function () {
       errorText.classList.remove("d-none");
     }
 
-    // Progress bar'ı gizle
+    // Progress bar'ı ve mesajları gizle
     progressContainer.style.display = "none";
-    progressTexts.style.display = "none";
+    progressMessage.style.display = "none";
+
     // Ana içerik alanlarının hepsinin görünür olmasını sağla
-    mainContent.forEach((content) => content.classList.remove("d-none")); // Ana içeriği görünür yap
+    mainContent.forEach((content) => content.classList.remove("d-none"));
   }
 
   function animateProgressBar(callback) {
     let width = 0;
+    const totalDuration = messages.length * 3000; // Toplam süre (mesaj başına 3 saniye)
     const interval = setInterval(() => {
       if (width >= 100) {
         clearInterval(interval);
         callback();
       } else {
-        width++;
+        width += 100 / (totalDuration / 60); // İlerleme hızı
         progressBar.style.width = width + "%";
         progressBar.setAttribute("aria-valuenow", width);
       }
-    }, 30); // 30ms'de bir artış
+    }, 60);
+
+    let messageIndex = 0;
+    let messageInterval = setInterval(() => {
+      if (messageIndex < messages.length) {
+        progressMessage.classList.add("fade-out");
+        setTimeout(() => {
+          progressMessage.innerText = messages[messageIndex];
+          progressMessage.classList.remove("fade-out");
+          progressMessage.classList.add("fade-in");
+          setTimeout(() => {
+            progressMessage.classList.remove("fade-in");
+          }, 500);
+          messageIndex++;
+        }, 500);
+      } else {
+        clearInterval(messageInterval);
+      }
+    }, 3000);
   }
 
   function checkServiceStatus(url) {
