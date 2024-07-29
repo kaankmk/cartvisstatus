8683ebe41d6285aadc5967e38ed58642bf0ad086
kaankmk <kaan@kmk.net.tr>
2024-07-29 15:55:28 +0300

    finito

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const errorIcon = statusContainer.querySelector(".error-icon");
   const successText = statusContainer.querySelector(".success-text");
   const errorText = statusContainer.querySelector(".error-text");
+  const progressBar = document.getElementById("progress-bar");
+  const progressTexts = document.querySelector(".progress-texts");
+  const progressContainer = progressBar.parentNode;
+  const mainContent = document.querySelectorAll(
+    "#main-content-1, #main-content-2, #main-content-3"
+  ); // Tüm içerik alanları
+
+  let dotsInterval;
+
+  // Başlangıçta tüm içerikleri gizle
+  mainContent.forEach((content) => content.classList.add("d-none"));
 
   function updateStatus(isSuccess) {
     if (isSuccess) {
@@ -17,25 +28,50 @@ document.addEventListener("DOMContentLoaded", function () {
       successText.classList.add("d-none");
       errorText.classList.remove("d-none");
     }
+
+    // Progress bar'ı gizle
+    progressContainer.style.display = "none";
+    progressTexts.style.display = "none";
+    // Ana içerik alanlarının hepsinin görünür olmasını sağla
+    mainContent.forEach((content) => content.classList.remove("d-none")); // Ana içeriği görünür yap
+  }
+
+  function animateProgressBar(callback) {
+    let width = 0;
+    const interval = setInterval(() => {
+      if (width >= 100) {
+        clearInterval(interval);
+        callback();
+      } else {
+        width++;
+        progressBar.style.width = width + "%";
+        progressBar.setAttribute("aria-valuenow", width);
+      }
+    }, 30); // 30ms'de bir artış
   }
 
   function checkServiceStatus(url) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://cors.bridged.cc/" + url, true);
+    xhr.timeout = 5000; // 5 saniye zaman aşımı süresi
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4) {
         console.log("HTTP Status: " + xhr.status); // HTTP durum kodunu loglayın
         console.log("Response: " + xhr.responseText); // Yanıtı loglayın
         if (xhr.status >= 200 && xhr.status < 300) {
-          updateStatus(true);
+          animateProgressBar(() => updateStatus(true));
         } else {
-          updateStatus(false);
+          animateProgressBar(() => updateStatus(false));
         }
       }
     };
+    xhr.ontimeout = function () {
+      console.error("Request Timed Out"); // Zaman aşımı hatasını loglayın
+      animateProgressBar(() => updateStatus(false));
+    };
     xhr.onerror = function () {
       console.error("Request Error: ", xhr); // Hata mesajını loglayın
-      updateStatus(false);
+      animateProgressBar(() => updateStatus(false));
     };
     xhr.send();
   }
